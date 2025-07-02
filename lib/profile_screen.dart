0f1176a59c98dde6c90fb998decb91aff43f2d7e
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2025-07-02 15:29:00 +0100

    feat:profile picture added

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,8 +1,19 @@
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  XFile? _imageFile;
+
+  final ImagePicker _picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
@@ -11,91 +22,167 @@ class ProfileScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: ListView(
           children: [
-            nameTextField(), 
-            SizedBox(height: 20,),
+            imageProfile(context),
+            SizedBox(height: 20),
+            nameTextField(),
+            SizedBox(height: 20),
             professionTextField(),
-             SizedBox(height: 20,),
+            SizedBox(height: 20),
             dobField(),
-             SizedBox(height: 20,),
+            SizedBox(height: 20),
             titleTextField(),
-             SizedBox(height: 20,),
+            SizedBox(height: 20),
             aboutTextField(),
-               SizedBox(height: 20,),
+            SizedBox(height: 20),
           ],
         ),
       ),
     );
   }
-  
 
-  Widget nameTextField(){
+  Widget bottomSheet(BuildContext context) {
+    return Container(
+      height: 100,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+        children: [
+          Text('Choose profile photo', style: TextStyle(fontSize: 20)),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                onPressed: () {
+                  takePhoto(ImageSource.camera);
+                },
+                icon: Icon(Icons.camera),
+                label: Text('Camera'),
+              ),
+
+              TextButton.icon(
+                onPressed: () {
+                  takePhoto(ImageSource.gallery);
+                },
+                icon: Icon(Icons.image),
+                label: Text('Gallery'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget imageProfile(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: [
+          CircleAvatar(
+            radius: 80,
+            backgroundImage:
+                _imageFile == null
+                    ? AssetImage('assets/profile_img.jpg') as ImageProvider
+                    : FileImage(File(_imageFile!.path)) as ImageProvider,
+          ),
+
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: InkWell(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: ((builder) => bottomSheet(context)),
+                );
+              },
+              child: Icon(Icons.camera_alt, color: Colors.teal, size: 20),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void takePhoto(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(source: source);
+    setState(() {
+      _imageFile = pickedFile;
+    });
+  }
+
+  Widget nameTextField() {
     return TextFormField(
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.teal)
+        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.orange, width: 2),
         ),
-        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange, width: 2)),
-        prefixIcon: Icon(Icons.person, color: Colors.green,),
+        prefixIcon: Icon(Icons.person, color: Colors.green),
         labelText: 'Name',
         helperText: 'Name cant be empty',
-        hintText: 'Dev Stack'
+        hintText: 'Dev Stack',
       ),
     );
   }
-  Widget professionTextField(){
+
+  Widget professionTextField() {
     return TextFormField(
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.teal)
+        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.orange, width: 2),
         ),
-        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange, width: 2)),
-        prefixIcon: Icon(Icons.person, color: Colors.green,),
+        prefixIcon: Icon(Icons.person, color: Colors.green),
         labelText: 'Profession',
         helperText: 'Profession cant be empty',
-        hintText: 'Full Stack Developer'
+        hintText: 'Full Stack Developer',
       ),
     );
   }
-  Widget dobField(){
+
+  Widget dobField() {
     return TextFormField(
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.teal)
+        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.orange, width: 2),
         ),
-        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange, width: 2)),
-        prefixIcon: Icon(Icons.person, color: Colors.green,),
+        prefixIcon: Icon(Icons.person, color: Colors.green),
         labelText: 'Date of Birth',
         helperText: 'Provide DOB on dd/mm/yyyy',
-        hintText: '01/01/2020'
+        hintText: '01/01/2020',
       ),
     );
   }
-  Widget titleTextField(){
+
+  Widget titleTextField() {
     return TextFormField(
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.teal)
+        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.orange, width: 2),
         ),
-        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange, width: 2)),
-        prefixIcon: Icon(Icons.person, color: Colors.green,),
+        prefixIcon: Icon(Icons.person, color: Colors.green),
         labelText: 'Title',
         helperText: 'It cant be empty',
-        hintText: 'Flutter Developer'
+        hintText: 'Flutter Developer',
       ),
     );
   }
-  Widget aboutTextField(){
+
+  Widget aboutTextField() {
     return TextFormField(
       maxLines: 4,
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.teal)
+        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.orange, width: 2),
         ),
-        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange, width: 2)),
-        
+
         labelText: 'About',
         helperText: 'Write about yourself',
-        hintText: ' I am Dev Stack'
+        hintText: ' I am Dev Stack',
       ),
     );
   }
